692dfc3bdc5be2bff9a5c5d7958d7d1b9dc925f5
xsun89 <sunxin89@gmail.com>
2015-09-13 21:10:21 -0700

    a

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         Solutions s = new Solutions();
         int test[] = {1,1,2,3,3,3,2,4,1,4,4,5,2};
-        int ret = s.singleNumberII(test);
+        int ret = s.singleNumber2(test);
         System.out.println(ret);
+
+        int test3[] = {1,2,2,3,4,4,5,3};
+        ArrayList<Integer> ret3 = s.singleNumber3(test3);
+        for (int i = 0; i < ret3.size(); i++) {
+            System.out.println(ret3.get(i));
+        }
     }
 }
--- src/Solutions.java
+++ src/Solutions.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by xsun on 15-09-07.
  */
@@ -8,18 +10,18 @@ public class Solutions {
         }
 
         int n = A[0];
-        for(int i = 1; i < A.length; i++) {
+        for (int i = 1; i < A.length; i++) {
             n = n ^ A[i];
         }
 
         return n;
     }
 
-    boolean isBit1(int num, int index)  {
+    boolean isBit1(int num, int index) {
         num = num >> index;
-        if((num & 1) == 1){
+        if ((num & 1) == 1) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
@@ -27,14 +29,88 @@ public class Solutions {
     int singleNumberII(int[] A) {
         int n = A.length;
         int result = 0;
-        for(int i = 0; i < 32; ++i) {
+        for (int i = 0; i < 32; ++i) {
             int count = 0;
-            for(int j = 0; j < n; ++j) {
-                if(isBit1(A[j], i))
+            for (int j = 0; j < n; ++j) {
+                if (isBit1(A[j], i))
                     ++count;
             }
             result |= (count % 3) << i;
         }
         return result;
     }
+
+    int singleNumber2(int[] A) {
+        if (A.length == 0) {
+            return 0;
+        }
+
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < A.length; i++) {
+            b = a & (b ^ A[i]);
+            a = b | (a ^ A[i]);
+        }
+
+        return a;
+    }
+
+    ArrayList<Integer> singleNumber3(int[] A) {
+        if (A.length == 0) {
+            return null;
+        }
+
+        int fstRet = A[0];
+        for (int i = 1; i < A.length; i++) {
+            fstRet = fstRet ^ A[i];
+        }
+
+        int msk = 0;
+        int fstBit1Index = 0;
+        for (; fstBit1Index < 32; fstBit1Index++) {
+            if (isBit1(fstRet, fstBit1Index)) {
+                break;
+            }
+        }
+        //System.out.println(fstBit1Index);
+        msk = 1 << fstBit1Index;
+        System.out.println(fstRet - (fstRet & (fstRet - 1)));
+        System.out.println(msk);
+        int groupFst = 0;
+        int groupSnd = 0;
+        for (int i = 0; i < A.length; i++) {
+            if ((msk & A[i]) == 0) {
+                groupFst = groupFst ^ A[i];
+            } else
+                groupSnd = groupSnd ^ A[i];
+        }
+
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        ret.add(groupFst);
+        ret.add(groupSnd);
+
+        return ret;
+    }
+
+    public int majorityNumber(ArrayList<Integer> nums) {
+        if(nums == null || nums.size() == 0){
+            return Integer.MIN_VALUE;
+        }
+        int number = Integer.MIN_VALUE;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if(count == 0){
+                number = nums.get(i);
+                count = 1;
+            }
+            else if(nums.get(i) == number){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        return number;
+    }
 }
+
